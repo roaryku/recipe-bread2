b3c8d041f74e0020ad569b20747aed886f66ead9
Rodika Aryku <raryku@yahoo.com>
2024-05-03 10:18:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import { useEffect, useState } from 'react';
+import { useCallback } from 'react';
+import video from './bread.mp4';
+import icon from './icons8.png'
+import MyRecipesComponent from './MyRecipesComponent';
+import { LoaderPage } from './LoaderPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+  const MY_ID = "fbf1aefa";
+  const MY_KEY = "1c7e7a9e2f62ef5ccd608ced66f4e2c3";
+
+  const [mySearch, setMySearch] = useState('');
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [wordSubmitted, setWordSubmitted] = useState('bread');
+  const [stateLoader, setStateLoader] = useState(true);
+
+  
+  
+    const getNewRecipe = useCallback(async () => {
+      const responce = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
+      const data = await responce.json();
+      setMyRecipes(data.hits);
+      setStateLoader(false)
+    },[wordSubmitted])
+    useEffect(() =>{
+        // if(wordSubmitted !== ''){
+        //   alert('Enter an item')
+        // } else{
+        //   getNewRecipe();
+        // }
+         getNewRecipe();
+      }, [getNewRecipe])
+
+    // useEffect(() => {
+    //   const timer = setTimeout(() => setStateLoader(false, 3000));
+    //      return() => clearTimeout(timer)
+    // }, [])
+                                                                       
+
+const myRecipeSearch = (e) => {
+  setMySearch(e.target.value);
 }
 
+const finalSearch = (e) => {
+  e.preventDefault();
+  setWordSubmitted(mySearch);
+}
+
+  return(
+    <div className="App">
+      {stateLoader && <LoaderPage/>}
+
+      <div className='container'>
+        <video autoPlay muted loop>
+          <source src={video} type="video/mp4"/>
+        </video>
+        <h1>Find your bread recipe</h1>
+    </div>
+      
+      
+      <div className='container heading'>
+        <form onSubmit = {finalSearch}>
+          <input className='search' placeholder='search...' onChange={myRecipeSearch} value={mySearch}></input>
+
+        <div className='container'>
+          <button className='btn'>
+            <img src={icon} width="35px" className='icons' alt='baking'/>
+          </button>
+          </div>
+        </form>
+      </div>
+      
+      <div>
+        {myRecipes.map((element, index) => (
+          <MyRecipesComponent 
+          key={index}
+          label = {element.recipe.label}
+          cuisine = {element.recipe.cuisineType}
+          image = {element.recipe.image}
+          ingredients = {element.recipe.ingredientLines}
+          calories = {element.recipe.calories}
+          diet = {element.recipe.dietLabels}
+          time = {element.recipe.totalTime}
+          />
+        ))}
+      </div>
+    </div>
+  
+  )
+}
 export default App;
---
+++ src/Loader.js
@@ -0,0 +1,9 @@
+export const Loader = () => {
+    return (
+        <div class="lds-facebook">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
